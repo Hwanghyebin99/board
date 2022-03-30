4ea5d35b62c4405603e6fd5c6797cb39da5ea577
황혜빈 <hebin99@naver.com>
2022-03-31 07:05:28 +0900

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardComponent } from './board/board.component';
 
-const routes: Routes = [];
+/**
+ * App 라우팅 정보
+ */
+ const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'board'
+  },
+  {
+    path:'board', component:BoardComponent
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
